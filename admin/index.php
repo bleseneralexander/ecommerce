5230a9f79d30231b57e011f9b4f290aeb61a445c
chuvoddolyhood <trannhannghia.24012000@gmail.com>
2021-09-23 09:37:03 +0700

    update folder

--- admin/index.php
+++ admin/index.php
@@ -1,52 +1,28 @@
-<?php
-    require_once './../connectSQL.php';
-?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <title>Westside Sneaker Store</title>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="keywords" content="Westside Sneaker Store, Sneaker, Shoes">
-    <meta name="description" content="ecommerce web">
-    <meta name="author" content="ch.studio" />
-    <link rel="shortcut icon" type="image/png" href="./../photo/logo.jpg" />
-
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-
-    
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous"> -->
+	<!-- <link rel="stylesheet" type="text/css" href="style/index.css" />
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" /> -->
 </head>
 <body>
-    <?php
-        if(isset($_GET['page_layout'])){
-            switch ($_GET['page_layout']) {
-                case 'login':
-                    require_once 'login.html';
-                    break;
+<?php
+	session_start();
+	//session_destroy();
+	if(!isset($_SESSION['dangnhapadmin'])){
+		header('location:login.php');
+	}
+?>
+<div class="wrapper">
+	<?php
+		include 'modules_admin/config.php';
+		include 'modules_admin/header.php';
+		include 'modules_admin/menu.php';
+		include 'modules_admin/content.php';
+		include 'modules_admin/footer.php';
+	?>
+</div>
 
-                case 'dashboard':
-                    require_once 'dashboard.html';
-                    break;
-
-                // case 'modify':
-                //     require_once 'sanpham/modify.php';
-                //     break;
-
-                // case 'delete':
-                //     require_once 'sanpham/delete.php';
-                //     break;
-
-                default:
-                    require_once 'login.html';
-                    break;
-            }
-        }else {
-            require_once 'login.html';
-        }
-    ?>
 </body>
 </html>
--- admin/login.php
+++ admin/login.php
@@ -1,8 +1,40 @@
-<!-- <!DOCTYPE html>
+<?php
+    include './modules_admin/config.php';
+    session_start();
+    if(isset($_POST['btnLogin'])){
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        if($_POST['username'] == '' || $_POST['password'] == ''){
+            echo 'Xin nhap du thong tin';
+        } else {
+            $sql = "SELECT password FROM nhanvien WHERE UserName='$username'";
+            $query = mysqli_query($conn, $sql);
+            $rows = mysqli_fetch_array($query);
+
+            if($rows['password'] == $password){
+                header('location: ./modules_admin/dashboard/dashboard.php');
+                // header('location: index.php');
+            } else {
+                header('location: login.php');
+            }   
+        }
+    }
+?>
+
+<!DOCTYPE html>
 <html>
 
 <head>
-    <link rel="stylesheet" href="login.css">
+    <title>Admin Page</title>
+    <link rel="stylesheet" href="./style/login.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="Westside Sneaker Store, Sneaker, Shoes">
+    <meta name="description" content="ecommerce web">
+    <meta name="author" content="ch.studio" />
+    <link rel="shortcut icon" type="image/png" href="./photo/logo.jpg" />
 </head>
 
 <body>
@@ -12,7 +44,7 @@
             <div class="main">
                 <div class="content">
                     <h2>Đăng nhập</h2>
-                    <form action="login_submit.php" method="POST">
+                     <form action="" method="POST">
                         <input type="text" name="username" placeholder="Tên đăng nhập" required autofocus="">
                         <input type="password" name="password" placeholder="Mật khẩu" required autofocus="">
                         <button class="btn_Login" type="submit" name="btnLogin">Đăng nhập</button>
@@ -20,11 +52,11 @@
                     
                 </div>
                 <div class="form-img">
-                    <img src="./../photo/teamNKC.png" alt="poster">
+                    <img src="./photo/teamNKC.png" alt="poster">
                 </div>
             </div>
         </div>
         <p class="developer">Được phát triển bởi <a href="https://chuvoddolyhood.github.io/trannhannghia/">ch.studio</a></p>
     </div>
 </body>
-</html> -->
+</html>
